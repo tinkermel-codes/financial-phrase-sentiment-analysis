ad3391789a64f32549050e512d009068f82f6271
Melanie <st145255@stud.uni-stuttgart.de>
2026-04-22 13:21:42 +0200

    Removed hardcoded values from train functions

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -3,12 +3,13 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from src.data.clean_data import cleaning_transformer
 
 
-def build_tfidf_vectorizer():
-    return TfidfVectorizer(max_features=3000, ngram_range=(1, 2))
+def build_tfidf_vectorizer(max_features, ngram_range):
+    #return TfidfVectorizer(max_features=3000, ngram_range=(1, 2))
+    return TfidfVectorizer(max_features=max_features, ngram_range=ngram_range)
 
 
-def build_pipeline(model, stop_words=None, lemmatizer=None):
-    tfidf = build_tfidf_vectorizer()
+def build_pipeline(model, stop_words, lemmatizer, max_features, ngram_range):
+    tfidf = build_tfidf_vectorizer(max_features, ngram_range)
 
     return Pipeline([
         ("clean", cleaning_transformer(stop_words, lemmatizer)),
@@ -17,7 +18,7 @@ def build_pipeline(model, stop_words=None, lemmatizer=None):
     ])
 
 
-def train_model(model, X_train, y_train, stop_words=None, lemmatizer=None):
-    pipeline = build_pipeline(model, stop_words, lemmatizer)
+def train_model(model, X_train, y_train, stop_words, lemmatizer, max_features, ngram_range):
+    pipeline = build_pipeline(model, stop_words, lemmatizer, max_features, ngram_range)
     pipeline.fit(X_train, y_train)
     return pipeline
