3803c91306c6914424a465edac1196f781853ec5
Melanie <st145255@stud.uni-stuttgart.de>
2026-04-30 13:25:38 +0200

    Added normalization for generating confusion matrices

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -7,6 +7,6 @@ def evaluate_model(pipeline, X_test, y_test):
     report_dict = classification_report(y_test, y_pred, output_dict=True)
     print(classification_report(y_test, y_pred))
 
-    cm = confusion_matrix(y_test, y_pred)
+    cm = confusion_matrix(y_test, y_pred, normalize="true")
     cm_df = pd.DataFrame(cm, index=pipeline.named_steps["clf"].classes_, columns=pipeline.named_steps["clf"].classes_)
     return report_dict, cm_df
